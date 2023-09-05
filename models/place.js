4ba3d5cb7efa3c5184e6973540ac0a34115688ef
Paul Mc Gonigle <77997276+paulmcgonigle1@users.noreply.github.com>
2023-09-05 10:12:49 +0200

    setting up of placeslist

---
+++ models/place.js
@@ -0,0 +1,8 @@
+class Place {
+  constructor(title, imageUri, address, location) {
+    this.title = title;
+    this.imageUri = imageUri;
+    this.address = address;
+    this.location = location; // {lat: 0.14121, lng: 127.121}
+  }
+}
